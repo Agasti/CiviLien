52e082b25ea36922ce5ee4282eb5b01499bc0dd5
Sehail Fillali <sehail.fillali@gmail.com>
2015-12-04 22:45:08 +0000

    Working location in map with permission handling

--- app/src/main/java/com/civilien/app/MapsActivity.java
+++ app/src/main/java/com/civilien/app/MapsActivity.java
@@ -1,15 +1,17 @@
 package com.civilien.app;
 
-import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -23,7 +25,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        Intent intent = getIntent();
     }
 
 
@@ -36,13 +37,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        permissionHandler();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-}
+
+    //Handles permission and messages
+    final static int RequestCodeAskPermission  = 123;
+    @TargetApi(Build.VERSION_CODES.M)
+    private void permissionHandler() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Toast.makeText(MapsActivity.this, "Please grant the app access to location services in \"Settings > APPS > Configure Apps > App Permissions > Location\"", Toast.LENGTH_LONG).show();
+                return;
+            }
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, RequestCodeAskPermission);
+        } else {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    // Permission callback
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case RequestCodeAskPermission:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    mMap.setMyLocationEnabled(true);
+                    Toast.makeText(MapsActivity.this, "Location access granted!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(MapsActivity.this, "Location access denied!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+    }
